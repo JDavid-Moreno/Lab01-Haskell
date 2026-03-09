cfaf169a13c0e7c35c41ad1437bc69eb2149e93d
unknown <juan.mdaleman@labinfo.is.escuelaing.edu.co>
2026-03-09 07:41:34 -0500

    feature: ejercicio01

---
+++ lab01/ejercicio01.hs
@@ -0,0 +1,30 @@
+--1) concatenar dos listas
+
+main = print(concatenar [1,2,3] [4,5])
+
+concatenar a b = a ++ b
+
+--Output: [1,2,3,4,5]
+
+--a)
+:t True
+-- True :: Bool
+
+--b)
+:t "Hola mundo"
+--"Hola mundo" :: String
+
+--c)
+:t [1,2,3]
+--[1,2,3] :: forall {a}. Num a => [a]
+
+-- Pregunta teórica: ¿Qué significa que Haskell sea un lenguaje fuertemente tipado y evaluado perezosamente?
+
+-- porque haskell es un lenguaje que garantiza mucha seguridad al compilar osea fuertemente tipado 
+-- y maxima eficiencia al evitar calculos innecesarios osea evaluacion perezosa 
+
+
+
+
+
+
