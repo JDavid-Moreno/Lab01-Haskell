d4ce49ca68593b32d4db37772859336c079b5b52
unknown <juan.mdaleman@labinfo.is.escuelaing.edu.co>
2026-03-09 09:58:28 -0500

    feature: ejercicio 1 corregido

---
+++ lab01/ejercicio04.hs
@@ -0,0 +1,18 @@
+--1)
+--a) prediccion: [2,4,6,8]
+-- ya que itera hasta antes de cinco por el take, y por el map multiplica 
+-- por 2 cada valor
+
+--b) ejecutado: [2,4,6,8,10]
+-- me falto el 10, no me acordaba que se contaba el 10, pense que uno antes
+
+--Pregunta de análisis: ¿Por qué [1..] no causa un ciclo infinito?
+-- porque gracias al take este no sigue avanzando hasta el infinito
+
+-- 2)
+-- a) misterio (5+7) (4*3)
+-- Output: 24
+-- primero se evalua x que es 12 y se mira si es mayor a 10, despues si 
+-- fue verdad entonces devuelve y * 2 que es 12, osea da como resultado 24
+
+ 
